ddb75eb1300f5b7d7bbb90cb65930dcbf94203f7
fe705 <maria.f.almeida@iesb.edu.br>
2025-03-06 22:29:12 -0300

    exercicio5

---
+++ exercicio5.js
@@ -0,0 +1,13 @@
+function calcularOperacoes(numero) {
+  const dobro = numero * 2;
+  const triplo = numero * 3;
+  const quadrado = numero * numero;
+
+  console.log("Número: " + numero);
+  console.log("Dobro: " + dobro);
+  console.log("Triplo: " + triplo);
+  console.log("Quadrado: " + quadrado);
+}
+
+const numero = 5;
+calcularOperacoes(numero);
