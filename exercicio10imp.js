08f0ace2ca67da4b2d44200231d7e14d9124221c
fe705 <maria.f.almeida@iesb.edu.br>
2025-03-06 22:52:29 -0300

    exercicio10imp

---
+++ exercicio10imp.js
@@ -0,0 +1,7 @@
+const calcularAreaCirculo = require("./calculadoraArea");
+
+const raio = 5;
+
+const area = calcularAreaCirculo(raio);
+
+console.log(`A área do círculo com raio ${raio} é: ${area}`);
